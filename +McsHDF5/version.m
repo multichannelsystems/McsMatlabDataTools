62b0cd04f45874631b69ece8a58d1f3b213a8676
Armin Walter <walter@multichannelsystems.com>
2015-04-23 10:00:20 +0200

    Added version function

---
+++ +McsHDF5/version.m
@@ -0,0 +1,7 @@
+function version()
+
+    fprintf(['This is version 0.3.0 of the McsHDF5 Matlab toolbox. (23/04/2015)\n'...
+        'Newer versions will be available at \n\n'...
+        '\thttp://www.multichannelsystems.com/software/multi-channel-datamanager \n']);
+
+end
